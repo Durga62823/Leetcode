4f05b94da21ead00d3137fbbbb05c93f52b3a25a
Peddapalli Satya Venkata Siva Durga Prasad <139907463+Durga62823@users.noreply.github.com>
2026-08-04 16:06:26 +0530

    Time: 0 ms (100%), Space: 19.3 MB (68.47%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -3,16 +3,25 @@ class Solution:
         n=len(nums)
         if n==1:
             return nums[0]
-        def solve(i,n,dp):
+        # def solve(i,n,dp):
             
-            if i>n-1:
-                return 0
-            if dp[i]!=-1:
-                return dp[i]
-            pick=nums[i]+solve(i+2,n,dp)
-            not_pick=solve(i+1,n,dp)
-            dp[i]= max(pick,not_pick)
+        #     if i>n-1:
+        #         return 0
+        #     if dp[i]!=-1:
+        #         return dp[i]
+        #     pick=nums[i]+solve(i+2,n,dp)
+        #     not_pick=solve(i+1,n,dp)
+        #     dp[i]= max(pick,not_pick)
+        #     return dp[i]
+        # dp1=[-1]*(n+2)
+        # dp2=[-1]*(n+2)
+        # return max(solve(0,n-1,dp1),solve(1,n,dp2))
+        def solve(i,n):
+            dp=[0]*(len(nums)+2)
+            for j in range(n,i-1,-1):
+                pick=nums[j]+dp[j+2]
+                not_pick=dp[j+1]
+                dp[j]=max(pick,not_pick)
+                
             return dp[i]
-        dp1=[-1]*(n+2)
-        dp2=[-1]*(n+2)
-        return max(solve(0,n-1,dp1),solve(1,n,dp2))
+        return max(solve(1,n-1),solve(0,n-2))
