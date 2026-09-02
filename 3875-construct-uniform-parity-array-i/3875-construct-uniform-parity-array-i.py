e19ab0cf903ca5a60db44c105cd25d9a582a1887
Peddapalli Satya Venkata Siva Durga Prasad <139907463+Durga62823@users.noreply.github.com>
2026-09-02 15:01:38 +0530

    3875-construct-uniform-parity-array-i Easy .py 0 ms (100%) 19.2 MB (84.34%) 09-02-2026

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -0,0 +1,7 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        # for i in range(len(nums1)):
+        #     if i+1<len(nums1) and (nums1[i]-nums[i+1])%2==1:
+        return True
+                
+                
